4156c6dd9633e08906bbd6affcb00229449c6db2
Cory Stegelmeier <cstegel@gmail.com>
2016-05-11 15:21:02 -0400

    added tests for ECS iteration

--- tests/integration/ecs.cc
+++ tests/integration/ecs.cc
@@ -63,29 +63,35 @@ namespace test
 		}
 	};
 
-	class EcsBasic : public ::testing::Test
+	// class EcsBasic : public ::testing::Test
+	// {
+	// protected:
+	//     sp::EntityManager em;
+	//     sp::Entity e;
+	//
+	//     virtual void SetUp()
+	//     {
+	//         e = em.NewEntity();
+	//     }
+	// };
+
+
+	TEST(EcsBasic, CreateDestroyEntity)
 	{
-	protected:
-	    sp::EntityManager em;
-	    sp::Entity e;
+		sp::EntityManager em;
+	    sp::Entity e = em.NewEntity();
 
-	    virtual void SetUp()
-	    {
-	        e = em.NewEntity();
-	    }
-	};
-
-
-	TEST_F(EcsBasic, CreateDestroyEntity)
-	{
 	    EXPECT_TRUE(e.Valid());
 	    e.Destroy();
 
 	    EXPECT_FALSE(e.Valid());
 	}
 
-	TEST_F(EcsBasic, AddRemoveComponent)
+	TEST(EcsBasic, AddRemoveComponent)
 	{
+		sp::EntityManager em;
+	    sp::Entity e = em.NewEntity();
+
 		e.Assign<Position>();
 
 		ASSERT_TRUE(e.Has<Position>());
@@ -96,8 +102,11 @@ namespace test
 	    ASSERT_ANY_THROW(e.Get<Position>());
 	}
 
-	TEST_F(EcsBasic, ConstructComponent)
+	TEST(EcsBasic, ConstructComponent)
 	{
+		sp::EntityManager em;
+	    sp::Entity e = em.NewEntity();
+
 	    e.Assign<Position>(1, 2);
 	    Position *pos = e.Get<Position>();
 
@@ -106,8 +115,11 @@ namespace test
 	    ASSERT_EQ(pos->y, 2);
 	}
 
-	TEST_F(EcsBasic, RemoveAllComponents)
+	TEST(EcsBasic, RemoveAllComponents)
 	{
+		sp::EntityManager em;
+	    sp::Entity e = em.NewEntity();
+
 	    e.Assign<Position>();
 	    e.Assign<Eater>();
 
@@ -139,5 +151,86 @@ namespace test
 		}
 
 		ExpectEntitiesFound();
+	}
+
+	TEST(EcsBasic, AddEntitiesWhileIterating)
+	{
+		sp::EntityManager em;
+		sp::Entity e1 = em.NewEntity();
+		e1.Assign<Position>();
+
+		int entitiesFound = 0;
+		for (sp::Entity ent : em.EntitiesWith<Position>())
+		{
+			entitiesFound++;
+			if (entitiesFound == 1)
+			{
+				sp::Entity e2 = em.NewEntity();
+				e2.Assign<Position>();
+			}
+		}
+
+		ASSERT_EQ(1, entitiesFound) << "Should have only found the entity created before started iterating";
+	}
+
+	// test to verify that an entity is not iterated over if it is destroyed
+	// before we get to it during iteration
+	TEST(EcsBasic, RemoveEntityWhileIterating)
+	{
+		sp::EntityManager em;
+
+		sp::Entity e1 = em.NewEntity();
+		e1.Assign<Position>();
+
+		sp::Entity e2 = em.NewEntity();
+		e2.Assign<Position>();
+
+		int entitiesFound = 0;
+		for (sp::Entity ent : em.EntitiesWith<Position>())
+		{
+			entitiesFound++;
+			if (ent == e1)
+			{
+				e2.Destroy();
+			}
+			else
+			{
+				e1.Destroy();
+			}
+		}
+
+		ASSERT_EQ(1, entitiesFound) <<
+			"Should have only found one entity because the other was destroyed";
+	}
+
+	// test to verify that an entity is not iterated over if it's component we are interested in
+	// is removed before we get to it during iteration
+	TEST(EcsBasic, RemoveComponentWhileIterating)
+	{
+		sp::EntityManager em;
+
+		sp::Entity e1 = em.NewEntity();
+		e1.Assign<Position>();
+
+		sp::Entity e2 = em.NewEntity();
+		e2.Assign<Position>();
+
+		int entitiesFound = 0;
+		for (sp::Entity ent : em.EntitiesWith<Position>())
+		{
+			entitiesFound++;
+			if (ent == e1)
+			{
+				e2.Remove<Position>();
+			}
+			else
+			{
+				e1.Remove<Position>();
+			}
+		}
+
+		ASSERT_EQ(1, entitiesFound) <<
+			"Should have only found one entity because the other's component was removed before"
+			" we got to it during iteration";
 	}
 }
