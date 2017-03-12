00fdf5f272669fbc849f505229d33ec1a43dbffb
Cory Stegelmeier <cstegel@gmail.com>
2017-03-12 15:47:14 -0400

    forgot to include a file in last commit

---
+++ src/ecs/components/Controller.cc
@@ -0,0 +1,18 @@
+#include "ecs/components/Controller.hh"
+
+#include <limits>
+
+namespace ecs
+{
+	void HumanController::SetRotate(const glm::quat &rotation)
+	{
+		pitch = glm::pitch(rotation);
+		yaw = glm::yaw(rotation);
+
+		if (std::abs(glm::roll(rotation)) > std::numeric_limits<float>::epsilon())
+		{
+			pitch += (pitch > 0) ? -M_PI : M_PI;
+			yaw = M_PI - yaw;
+		}
+	}
+}
