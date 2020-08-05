5bc4d05ca5c7d101e666d5279d205595b631a94b
Jacob Wirth <xthexder@users.noreply.github.com>
2020-08-05 03:00:25 -0700

    Switch to buildkite-agent manual upload

--- ci-build.sh
+++ ci-build.sh
@@ -79,6 +79,7 @@ for file in ../assets/scripts/tests/*.txt; do
     mkdir -p $output_path
     for file in screenshots/*.png; do
         mv $file $output_path
+        buildkite-agent artifact upload "$output_path/${file##*/}"
         inline_image "artifact://bin/$output_path/${file##*/}" "$output_path/${file##*/}"
     done
 done
