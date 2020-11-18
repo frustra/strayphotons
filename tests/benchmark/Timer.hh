3340b6ada7ffca2ed3ad75e302e017fa300954a0
Jacob Wirth <xthexder@users.noreply.github.com>
2020-11-17 23:41:29 -0800

    Add benchmark of Glomerate to compare with Tecs

--- tests/benchmark/Timer.hh
+++ tests/benchmark/Timer.hh
@@ -4,65 +4,82 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace benchmark {
-	class MultiTimer {
-	public:
-		MultiTimer(std::string name, bool print = true) : name(name), print(print) {
-			if (print) { std::cout << "[" << name << "] Start" << std::endl; }
-		}
+    class MultiTimer {
+    public:
+        MultiTimer(const MultiTimer &) = delete;
 
-		void AddValue(std::chrono::nanoseconds value) {
-			values.emplace_back(value);
-		}
+        MultiTimer(std::string name, bool print = true) : name(name), print(print) {
+            if (print) { std::cout << "[" << name << "] Start" << std::endl; }
+        }
 
-		~MultiTimer() {
-			if (print) {
-				if (values.size() >= 1) {
-					std::chrono::nanoseconds total(0);
-					for (auto value : values) {
-						total += value;
-					}
-					std::sort(values.begin(), values.end(), std::less());
+        void AddValue(std::chrono::nanoseconds value) {
+            values.emplace_back(value);
+        }
 
-					std::cout << "[" << name << "] Min: " << (values[0].count() / 1000.0)
-							  << " usec, Avg: " << (total.count() / values.size() / 1000.0)
-							  << " usec, P95: " << (values[(size_t)((double)values.size() * 0.95) - 1].count() / 1000.0)
-							  << " usec, P99: " << (values[(size_t)((double)values.size() * 0.99) - 1].count() / 1000.0)
-							  << " usec" << std::endl;
-				} else {
-					std::cout << "[" << name << "] No timers completed" << std::endl;
-				}
-			}
-		}
+        ~MultiTimer() {
+            if (print) {
+                if (values.size() > 1) {
+                    std::chrono::nanoseconds total(0);
+                    for (auto value : values) {
+                        total += value;
+                    }
+                    std::sort(values.begin(), values.end(), std::less<std::chrono::nanoseconds>());
 
-	private:
-		std::string name;
-		bool print;
-		std::vector<std::chrono::nanoseconds> values;
-	};
+                    std::cout << "[" << name << "] Min: " << (values[0].count() / 1000.0)
+                              << " usec, Avg: " << (total.count() / values.size() / 1000.0)
+                              << " usec, P95: " << (values[(size_t)((double)values.size() * 0.95) - 1].count() / 1000.0)
+                              << " usec, P99: " << (values[(size_t)((double)values.size() * 0.99) - 1].count() / 1000.0)
+                              << " usec" << std::endl;
+                } else if (values.size() == 1) {
+                    std::cout << "[" << name << "] End: " << (values[0].count() / 1000000.0) << " ms" << std::endl;
+                } else {
+                    std::cout << "[" << name << "] No timers completed" << std::endl;
+                }
+            }
+        }
 
-	class Timer {
-	public:
-		Timer(std::string name) : name(name) {
-			std::cout << "[" << name << "] Start" << std::endl;
-			start = std::chrono::high_resolution_clock::now();
-		}
-		Timer(MultiTimer &parent) : parent(&parent) {
-			start = std::chrono::high_resolution_clock::now();
-		}
-		~Timer() {
-			auto end = std::chrono::high_resolution_clock::now();
-			if (parent != nullptr) {
-				parent->AddValue(end - start);
-			} else {
-				std::cout << "[" << name << "] End: " << ((end - start).count() / 1000000.0) << " ms" << std::endl;
-			}
-		}
+    private:
+        std::string name;
+        bool print;
+        std::vector<std::chrono::nanoseconds> values;
+    };
 
-	private:
-		std::string name;
-		std::chrono::high_resolution_clock::time_point start;
-		MultiTimer *parent = nullptr;
-	};
+    class Timer {
+    public:
+        Timer(const Timer &) = delete;
+
+        Timer(std::string name) : name(name) {
+            std::cout << "[" << name << "] Start" << std::endl;
+            start = std::chrono::high_resolution_clock::now();
+        }
+        Timer(MultiTimer &parent) : parent(&parent) {
+            start = std::chrono::high_resolution_clock::now();
+        }
+
+        ~Timer() {
+            auto end = std::chrono::high_resolution_clock::now();
+            if (parent != nullptr) {
+                parent->AddValue(end - start);
+            } else if (!name.empty()) {
+                std::cout << "[" << name << "] End: " << ((end - start).count() / 1000000.0) << " ms" << std::endl;
+            }
+        }
+
+        Timer &operator=(MultiTimer &newParent) {
+            this->~Timer();
+            this->name = "";
+            this->parent = &newParent;
+            start = std::chrono::high_resolution_clock::now();
+
+            return *this;
+        }
+
+    private:
+        std::string name;
+        std::chrono::high_resolution_clock::time_point start;
+        MultiTimer *parent = nullptr;
+    };
 } // namespace benchmark
---
+++ tests/benchmark/test_components.hh
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Ecs.hh>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace benchmark {
+    struct Transform {
+        double pos[3] = {0};
+        ecs::Entity parent;
+
+        Transform() {}
+        Transform(double x, double y, double z) : parent() {
+            pos[0] = x;
+            pos[1] = y;
+            pos[2] = z;
+        }
+        Transform(double x, double y, double z, ecs::Entity parent) : parent(parent) {
+            pos[0] = x;
+            pos[1] = y;
+            pos[2] = z;
+        }
+
+        bool HasParent() {
+			return parent.Valid();
+		}
+    };
+
+    struct Script {
+        std::vector<uint8_t> data;
+
+        Script() {}
+        Script(uint8_t *data, size_t size) : data(data, data + size) {}
+        Script(std::initializer_list<uint8_t> init) : data(init) {}
+    };
+
+    struct Renderable {
+        std::string name;
+
+        Renderable() {}
+        Renderable(std::string name) : name(name) {}
+    };
+}; // namespace benchmark
