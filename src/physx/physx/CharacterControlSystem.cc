a1a932cc357679a25f4faea178b31c90f1f71836
Jacob Wirth <xthexder@users.noreply.github.com>
2021-10-19 23:54:13 -0600

    Update Tecs to 0.8.0 (#150)

--- src/physx/physx/CharacterControlSystem.cc
+++ src/physx/physx/CharacterControlSystem.cc
@@ -28,8 +28,7 @@ namespace sp {
 
     CharacterControlSystem::CharacterControlSystem(PhysxManager &manager) : manager(manager) {
         auto lock = ecs::World.StartTransaction<ecs::AddRemove>();
-        additionObserver = lock.Watch<ecs::Added<ecs::CharacterController>>();
-        removalObserver = lock.Watch<ecs::Removed<ecs::CharacterController>>();
+        characterControllerObserver = lock.Watch<ecs::ComponentEvent<ecs::CharacterController>>();
     }
 
     void CharacterControlSystem::Frame(double dtSinceLastFrame) {
@@ -37,42 +36,41 @@ namespace sp {
             ecs::Read<ecs::Name, ecs::SignalOutput, ecs::SignalBindings, ecs::FocusLayer, ecs::FocusLock>,
             ecs::Write<ecs::Transform, ecs::CharacterController>>();
 
-        ecs::Removed<ecs::CharacterController> removedEvent;
-        while (removalObserver.Poll(lock, removedEvent)) {
-            if (removedEvent.component.pxController) manager.RemoveController(removedEvent.component.pxController);
-        }
+        ecs::ComponentEvent<ecs::CharacterController> event;
+        while (characterControllerObserver.Poll(lock, event)) {
+            if (event.type == Tecs::EventType::ADDED) {
+                if (event.entity.Has<ecs::CharacterController>(lock)) {
+                    auto &controller = event.entity.Get<ecs::CharacterController>(lock);
+                    if (!controller.pxController) {
+                        PxCapsuleControllerDesc desc;
+                        desc.position = PxExtendedVec3(0, 0, 0);
+                        desc.upDirection = PxVec3(0, 1, 0);
+                        desc.radius = ecs::PLAYER_RADIUS;
+                        desc.height = ecs::PLAYER_CAPSULE_HEIGHT;
+                        desc.density = 0.5f;
+                        desc.material = manager.pxPhysics->createMaterial(0.3f, 0.3f, 0.3f);
+                        desc.climbingMode = PxCapsuleClimbingMode::eCONSTRAINED;
+                        desc.reportCallback = manager.controllerHitReporter.get();
+                        desc.userData = new CharacterControllerUserData();
 
-        ecs::Added<ecs::CharacterController> addedEvent;
-        while (additionObserver.Poll(lock, addedEvent)) {
-            if (addedEvent.entity.Has<ecs::CharacterController>(lock)) {
-                auto &controller = addedEvent.entity.Get<ecs::CharacterController>(lock);
-                if (!controller.pxController) {
-                    PxCapsuleControllerDesc desc;
-                    desc.position = PxExtendedVec3(0, 0, 0);
-                    desc.upDirection = PxVec3(0, 1, 0);
-                    desc.radius = ecs::PLAYER_RADIUS;
-                    desc.height = ecs::PLAYER_CAPSULE_HEIGHT;
-                    desc.density = 0.5f;
-                    desc.material = manager.pxPhysics->createMaterial(0.3f, 0.3f, 0.3f);
-                    desc.climbingMode = PxCapsuleClimbingMode::eCONSTRAINED;
-                    desc.reportCallback = manager.controllerHitReporter.get();
-                    desc.userData = new CharacterControllerUserData();
+                        auto pxController = manager.controllerManager->createController(desc);
+                        Assert(pxController->getType() == PxControllerShapeType::eCAPSULE,
+                            "Physx did not create a valid PxCapsuleController");
 
-                    auto pxController = manager.controllerManager->createController(desc);
-                    Assert(pxController->getType() == PxControllerShapeType::eCAPSULE,
-                        "Physx did not create a valid PxCapsuleController");
+                        pxController->setStepOffset(ecs::PLAYER_STEP_HEIGHT);
 
-                    pxController->setStepOffset(ecs::PLAYER_STEP_HEIGHT);
+                        PxShape *shape;
+                        pxController->getActor()->getShapes(&shape, 1);
+                        PxFilterData data;
+                        data.word0 = PhysxCollisionGroup::PLAYER;
+                        shape->setQueryFilterData(data);
+                        shape->setSimulationFilterData(data);
 
-                    PxShape *shape;
-                    pxController->getActor()->getShapes(&shape, 1);
-                    PxFilterData data;
-                    data.word0 = PhysxCollisionGroup::PLAYER;
-                    shape->setQueryFilterData(data);
-                    shape->setSimulationFilterData(data);
-
-                    controller.pxController = static_cast<PxCapsuleController *>(pxController);
+                        controller.pxController = static_cast<PxCapsuleController *>(pxController);
+                    }
                 }
+            } else if (event.type == Tecs::EventType::REMOVED) {
+                if (event.component.pxController) manager.RemoveController(event.component.pxController);
             }
         }
 
