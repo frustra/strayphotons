f5f3b14b070b2b0172d1c31dc244e008ee612181
Jacob Wirth <xthexder@users.noreply.github.com>
2021-12-21 21:03:12 -0500

    Add simple wasmer example

---
+++ rust/src/wasmer_vm.rs
@@ -0,0 +1,24 @@
+use wasmer::{Store, Module, Instance, Value, imports};
+
+pub fn run_wasm() -> anyhow::Result<i32> {
+    let module_wat = r#"
+    (module
+    (type $t0 (func (param i32) (result i32)))
+    (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+        get_local $p0
+        i32.const 1
+        i32.add))
+    "#;
+
+    let store = Store::default();
+    let module = Module::new(&store, &module_wat)?;
+    // The module doesn't import anything, so we create an empty import object.
+    let import_object = imports! {};
+    let instance = Instance::new(&module, &import_object)?;
+
+    let add_one = instance.exports.get_function("add_one")?;
+    let result = add_one.call(&[Value::I32(42)])?;
+    assert_eq!(result[0], Value::I32(43));
+
+    Ok(result[0].unwrap_i32())
+}
