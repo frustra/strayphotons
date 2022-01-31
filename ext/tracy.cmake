25a7d0554adcff6995781b1c696cc67def733b14
Justin Li <git@justinli.net>
2022-01-30 19:10:09 -0500

    Disable warnings in tracy

--- ext/tracy.cmake
+++ ext/tracy.cmake
@@ -1,2 +1,7 @@
 option(TRACY_ENABLE "" ON)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    add_compile_options(-Wno-unused-but-set-variable -Wno-array-bounds)
+endif()
+
 add_subdirectory(tracy)
