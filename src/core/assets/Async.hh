8c30725cba51c3501a7770f3538f0c363bbb463d
Jacob Wirth <xthexder@users.noreply.github.com>
2022-02-15 19:49:01 -0500

    Alternate sp::Async implementation

--- src/core/assets/Async.hh
+++ src/core/assets/Async.hh
@@ -12,26 +12,45 @@ namespace sp {
     template<typename T>
     class Async {
     public:
-        Async(std::future<std::shared_ptr<T>> &&future) : future(future.share()) {
+        Async(std::future<std::shared_ptr<T>> &&future) : future(std::move(future)) {
             Assertf(this->future.valid(), "sp::Async created with invalid future");
         }
-        Async(const std::shared_ptr<T> &ptr) {
-            future = std::async(std::launch::deferred, [ptr] {
-                return ptr;
-            }).share();
+        Async(const std::shared_ptr<T> &ptr) : value(ptr) {
+            valueSet.test_and_set();
         }
 
-        bool Ready() const {
-            return future.wait_for(std::chrono::seconds(0)) != std::future_status::timeout;
+        bool Ready() {
+            if (valueSet.test()) return true;
+
+            std::unique_lock lock(futureMutex, std::try_to_lock);
+            if (!lock) return false;
+            return !futureWaiting.test() && future.wait_for(std::chrono::seconds(0)) != std::future_status::timeout;
         }
 
-        std::shared_ptr<T> Get() {
+        std::shared_ptr<const T> Get() {
             Assertf(this != nullptr, "AsyncPtr->Get() called on nullptr");
-            return future.get();
+            if (!valueSet.test()) {
+                std::lock_guard lock(futureMutex);
+                if (!futureWaiting.test_and_set()) {
+                    value = future.get();
+                    valueSet.test_and_set();
+                    valueSet.notify_all();
+                    return value;
+                }
+            }
+
+            while (!valueSet.test()) {
+                valueSet.wait(false);
+            }
+            return value;
         }
 
     private:
-        std::shared_future<std::shared_ptr<T>> future;
+        std::atomic_flag valueSet, futureWaiting;
+        std::shared_ptr<const T> value;
+
+        LockFreeMutex futureMutex;
+        std::future<std::shared_ptr<T>> future;
     };
 
     template<typename T>
