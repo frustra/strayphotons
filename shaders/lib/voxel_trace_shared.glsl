24fa506f16faf7163e95f4c30ac9d42bef37682b
Jacob Wirth <xthexder@users.noreply.github.com>
2022-05-07 17:54:34 -0400

    Final diffuse code sample spacing tweaks

--- shaders/lib/voxel_trace_shared.glsl
+++ shaders/lib/voxel_trace_shared.glsl
@@ -34,37 +34,28 @@ vec4 ConeTraceGrid(float ratio, vec3 rayPos, vec3 rayDir, vec3 surfaceNormal, ve
     return result;
 }
 
-// const float[7] DiffuseSampleSpacing = float[](1.0, 1.5, 2.0, 3.0, 5.8, 12.0, 15.0);
-const float[7] DiffuseSampleSpacing = float[](2.0, 2.4, 8.0, 16.0, 32.8, 64.0, 128.0);
+const float[5] DiffuseSampleSpacing = float[](1.0, 1.8, 3.0, 8.0, 16.0);
 
 vec4 ConeTraceGridDiffuse(vec3 rayPos, vec3 rayDir, vec3 surfaceNormal, float offset) {
     vec3 voxelPos = (voxelInfo.worldToVoxel * vec4(rayPos, 1.0)).xyz;
     vec3 voxelDir = normalize(mat3(voxelInfo.worldToVoxel) * rayDir);
 
-    float dist = DiffuseSampleSpacing[0] + saturate(offset);
+    float dist = saturate(offset);
     vec4 result = vec4(0);
 
-    // TODO: Fix this constant
-    for (int level = 1; level < 5; level++) {
+    for (int level = 0; level < 5; level++) {
+        dist += DiffuseSampleSpacing[level];
+
         vec3 position = voxelPos + voxelDir * dist;
         vec4 value = textureLod(voxelRadiance, position / voxelInfo.gridSize, level);
         vec4 normal = textureLod(voxelNormals, position / voxelInfo.gridSize, level);
         normal.xyz /= max(0.001, normal.w);
         value.a *= step(0.001, normal.w);
 
-        value.rgb *= smoothstep(0.2, 0.8, length(normal.xyz));
-        value.a *= step(-0.1, dot(rayDir, -normal.xyz));
+        value.rgb *= smoothstep(0.1, 0.4, length(normal.xyz));
+        value.a *= step(-0.2, dot(rayDir, -normal.xyz));
 
-        // Bias the alpha to prevent traces going through objects.
-        // value *= linstep(0.0, 0.3, value.a) / max(0.001, value.a);
-        // if (level < 5) {
-        //     value.rgb = vec3(value.a);
-        // } else {
-        //     value.rgb = vec3(0);
-        // }
         result += vec4(value.rgb, value.a) * (1.0 - result.a) * (1 - step(0, -value.a));
-
-        dist += DiffuseSampleSpacing[level];
     }
 
     return result;
