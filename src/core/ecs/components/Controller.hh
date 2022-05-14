420ca8defb215bc0b43f8d7144e16b7c234f20ca
Jacob Wirth <xthexder@users.noreply.github.com>
2022-05-13 20:00:13 -0400

    More cleanup

--- src/core/ecs/components/Controller.hh
+++ src/core/ecs/components/Controller.hh
@@ -14,16 +14,13 @@ namespace physx {
 namespace ecs {
     // Units in meters
     const float PLAYER_HEIGHT = 1.8f;
-    const float PLAYER_CROUCH_HEIGHT = 0.8f;
     const float PLAYER_RADIUS = 0.2f;
-    const float PLAYER_CAPSULE_CROUCH_HEIGHT = PLAYER_CROUCH_HEIGHT - 2 * PLAYER_RADIUS;
     const float PLAYER_CAPSULE_HEIGHT = PLAYER_HEIGHT - 2 * PLAYER_RADIUS;
-    const float PLAYER_STEP_HEIGHT = 0.2f;
+    const float PLAYER_STEP_HEIGHT = 0.05f;
 
     const float PLAYER_GRAVITY = 9.81f;
     const float PLAYER_JUMP_VELOCITY = 4.0f;
     const float PLAYER_AIR_STRAFE = 0.8f; // Movement scaler for acceleration in air
-    const float PLAYER_PUSH_FORCE = 0.3f;
 
     struct CharacterController {
         EntityRef target, fallbackTarget, movementProxy;
