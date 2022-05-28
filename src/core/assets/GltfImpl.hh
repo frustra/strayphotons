45a0665e2bb5bf76beba65945758a480869ec657
Jacob Wirth <xthexder@users.noreply.github.com>
2022-05-28 14:50:18 -0400

    Update Tecs and get rid of windows.h include in tinygltf

--- src/core/assets/GltfImpl.hh
+++ src/core/assets/GltfImpl.hh
@@ -4,8 +4,19 @@
 
 #include <glm/glm.hpp>
 #include <memory>
-#include <tiny_gltf.h>
 #include <type_traits>
+
+// Hacky defines to prevent tinygltf from including windows.h and polluting the namespace
+#ifdef _WIN32
+    #define UNDEFINE_WIN32
+    #undef _WIN32
+    #define __ANDROID__
+#endif
+#include <tiny_gltf.h>
+#ifdef UNDEFINE_WIN22
+    #define _WIN32
+    #undef __ANDROID__
+#endif
 
 namespace sp::gltf {
     namespace detail {
