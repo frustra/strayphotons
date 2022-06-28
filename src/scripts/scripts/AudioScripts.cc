3f0f155a687ed079df49998247292121c65c273b
Jacob Wirth <xthexder@users.noreply.github.com>
2022-06-27 21:25:43 -0400

    Mirrors should block sound

--- src/scripts/scripts/AudioScripts.cc
+++ src/scripts/scripts/AudioScripts.cc
@@ -34,8 +34,9 @@ namespace ecs {
                     auto rayToListener = listenerPos - soundPos;
 
                     PhysicsQuery::Raycast nextQuery(glm::length(rayToListener),
-                        PhysicsGroupMask(PHYSICS_GROUP_WORLD | PHYSICS_GROUP_INTERACTIVE | PHYSICS_GROUP_HELD_OBJECT |
-                                         PHYSICS_GROUP_PLAYER_LEFT_HAND | PHYSICS_GROUP_PLAYER_RIGHT_HAND));
+                        PhysicsGroupMask(PHYSICS_GROUP_WORLD | PHYSICS_GROUP_WORLD_OVERLAP | PHYSICS_GROUP_INTERACTIVE |
+                                         PHYSICS_GROUP_HELD_OBJECT | PHYSICS_GROUP_PLAYER_LEFT_HAND |
+                                         PHYSICS_GROUP_PLAYER_RIGHT_HAND));
                     nextQuery.direction = glm::normalize(rayToListener);
                     nextQuery.relativeDirection = false;
                     nextQuery.maxHits = 16;
