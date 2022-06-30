632dd11a9fdb1d44efc1e4992ab5be4df186c81a
Jacob Wirth <xthexder@users.noreply.github.com>
2022-06-30 00:27:39 -0400

    Add wall prefab script to replace ejs template (#347)
    * Add wall prefab script to replace ejs template
    
    * Remove final ejs prefab
    
    * Clangformat
    
    * Convert some .scenes to plain .json

---
+++ src/scripts/prefabs/WallPrefab.cc
@@ -0,0 +1,83 @@
+#include "core/Common.hh"
+#include "core/Logging.hh"
+#include "ecs/EcsImpl.hh"
+#include "game/Scene.hh"
+#include "game/SceneManager.hh"
+
+#include <glm/glm.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+namespace ecs {
+    InternalPrefab wallPrefab("wall", [](ScriptState &state, Lock<AddRemove> lock, Entity ent) {
+        auto scene = state.scope.scene.lock();
+        Assertf(scene, "wall prefab does not have a valid scene: %s", ToString(lock, ent));
+        if (!state.HasParam<vector<double>>("segments_x") || !state.HasParam<vector<double>>("segments_y")) return;
+        if (!state.HasParam<vector<string>>("segment_types") || !state.HasParam<double>("stride")) return;
+
+        float yOffset = state.GetParam<double>("y_offset");
+        float stride = state.GetParam<double>("stride");
+
+        auto &segmentsX = state.GetParamRef<vector<double>>("segments_x");
+        auto &segmentsY = state.GetParamRef<vector<double>>("segments_y");
+        auto &segmentTypes = state.GetParamRef<vector<string>>("segment_types");
+
+        Assertf(segmentsX.size() == segmentsY.size() && segmentsX.size() == segmentTypes.size() + 1,
+            "wall prefab does not have equal length segment lists: %s, (%u, %u, %u)",
+            ToString(lock, ent),
+            segmentsX.size(),
+            segmentsY.size(),
+            segmentTypes.size());
+
+        glm::vec2 lastDir = glm::vec2(1, 0);
+        for (size_t segment = 1; segment < segmentsX.size(); segment++) {
+            glm::vec2 point(segmentsX[segment - 1], segmentsY[segment - 1]);
+            glm::vec2 nextPoint(segmentsX[segment], segmentsY[segment]);
+            glm::vec2 dir = nextPoint - point;
+            auto distance = glm::length(dir);
+            dir = glm::normalize(dir);
+            auto rotation = glm::orientedAngle(dir, glm::vec2(1, 0));
+
+            if (segment > 1 && dir != lastDir) {
+                auto newEnt = scene->NewPrefabEntity(lock, ent);
+
+                auto deltaRotation = glm::orientedAngle(lastDir, dir);
+                TransformTree transform(glm::vec3(point.x, yOffset, point.y),
+                    glm::quat(glm::vec3(0, rotation + deltaRotation / 2, 0)));
+                if (ent.Has<TransformTree>(lock)) transform.parent = ent;
+                Component<TransformTree>::Apply(transform, lock, newEnt);
+
+                Script scriptComp;
+                auto &gltfState = scriptComp.AddPrefab(state.scope, PrefabDefinitions["gltf"]);
+                gltfState.SetParam<std::string>("model", "wall-4-corner");
+                gltfState.SetParam<std::string>("physics", "static");
+                gltfState.SetParam<bool>("render", true);
+                Component<Script>::Apply(scriptComp, lock, newEnt);
+                newEnt.Get<Script>(lock).Prefab(lock, newEnt);
+            }
+            lastDir = dir;
+
+            std::string model = segmentTypes[segment - 1];
+
+            point += dir * stride * 0.5f;
+            size_t count = (size_t)std::floor(distance / stride);
+            for (size_t i = 0; i < count; i++) {
+                auto newEnt = scene->NewPrefabEntity(lock, ent);
+
+                TransformTree transform(glm::vec3(point.x, yOffset, point.y), glm::quat(glm::vec3(0, rotation, 0)));
+                if (ent.Has<TransformTree>(lock)) transform.parent = ent;
+
+                Component<TransformTree>::Apply(transform, lock, newEnt);
+
+                Script scriptComp;
+                auto &gltfState = scriptComp.AddPrefab(state.scope, PrefabDefinitions["gltf"]);
+                gltfState.SetParam<std::string>("model", model);
+                gltfState.SetParam<std::string>("physics", "static");
+                gltfState.SetParam<bool>("render", true);
+                Component<Script>::Apply(scriptComp, lock, newEnt);
+                newEnt.Get<Script>(lock).Prefab(lock, newEnt);
+
+                point += dir * stride;
+            }
+        }
+    });
+} // namespace ecs
