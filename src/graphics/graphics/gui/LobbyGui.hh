6a3fe9f0e587586cf2eb70dc19c51f81a3c1a4e3
Justin Li <git@justinli.net>
2022-07-06 18:48:16 -0400

    Slightly expand example gui

--- src/graphics/graphics/gui/LobbyGui.hh
+++ src/graphics/graphics/gui/LobbyGui.hh
@@ -10,10 +10,18 @@ namespace sp {
         LobbyGui(const string &name) : GuiWindow(name) {}
         virtual ~LobbyGui() {}
 
+        enum class State { Initial, Page1 };
+
+        State state = State::Initial;
+
         void DefineContents() {
             ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, {30, 20});
             PushFont(Font::Primary, 32);
-            ImGui::Button("Register");
+            if (state == State::Initial) {
+                if (ImGui::Button("Check In")) state = State::Page1;
+            } else if (state == State::Page1) {
+                ImGui::Text("Unimplemented");
+            }
             ImGui::PopFont();
             ImGui::PopStyleVar();
         }
