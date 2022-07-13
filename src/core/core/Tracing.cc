4fadcabe69cc24986eb85121660d104f90bb0e18
Jacob Wirth <xthexder@users.noreply.github.com>
2022-07-13 15:06:38 -0400

    Update tracy and fix GCC 12 build (#363)

--- src/core/core/Tracing.cc
+++ src/core/core/Tracing.cc
@@ -2,14 +2,14 @@
 
 #include <cstdlib>
 
-void *operator new(size_t size) {
-    auto *ptr = std::malloc(size);
-    // This holds a shared_mutex internally that causes multiple threads to block on malloc
-    // TracyAlloc(ptr, size);
-    return ptr;
-}
+// void *operator new(size_t size) {
+//     auto *ptr = std::malloc(size);
+//     // This holds a shared_mutex internally that causes multiple threads to block on malloc
+//     TracyAlloc(ptr, size);
+//     return ptr;
+// }
 
-void operator delete(void *ptr) {
-    // TracyFree(ptr);
-    std::free(ptr);
-}
+// void operator delete(void *ptr) {
+//     TracyFree(ptr);
+//     std::free(ptr);
+// }
