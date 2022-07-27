f6bf7280356c9ee226f3c2325c9b832876c4ecb5
Jacob Wirth <xthexder@users.noreply.github.com>
2022-07-27 18:29:54 -0400

    Update vhacd to latest version via submodule

--- ext/vhacd.cmake
+++ ext/vhacd.cmake
@@ -1,14 +1,3 @@
-option(NO_OPENCL "NO_OPENCL" ON)
-option(NO_OPENMP "NO_OPENMP" ON)
-set(CMAKE_COMMON_INC "../cmake_common.cmake")
-set(BUILD_SHARED_LIBS off)
+add_library(vhacd INTERFACE)
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "8.0.0")
-        add_compile_options(-Wno-stringop-overflow -Wno-class-memaccess -Wno-unused-result -Wno-maybe-uninitialized -Wno-unknown-pragmas -Wno-misleading-indentation -Wno-strict-aliasing)
-    endif()
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    add_compile_options(-Wno-unknown-pragmas -Wno-misleading-indentation)
-endif()
-
-add_subdirectory(v-hacd/VHACD_Lib)
+target_include_directories(vhacd INTERFACE v-hacd/include)
--- src/physx/physx/ConvexHull.cc
+++ src/physx/physx/ConvexHull.cc
@@ -4,14 +4,13 @@
 #include "assets/GltfImpl.hh"
 #include "core/Logging.hh"
 
-#include <v-hacd/VHACD_Lib/inc/btConvexHullComputer.h>
-#include <v-hacd/VHACD_Lib/inc/vhacdICHull.h>
-#include <v-hacd/VHACD_Lib/inc/vhacdVector.h>
-#include <v-hacd/VHACD_Lib/public/VHACD.h>
+#include <unordered_set>
+
+#define ENABLE_VHACD_IMPLEMENTATION 1
+#include <VHACD.h>
 
 #define GLM_FORCE_CXX11
 #include <glm/gtx/hash.hpp>
-#include <unordered_set>
 
 namespace sp {
     class VHACDCallback : public VHACD::IVHACD::IUserCallback {
@@ -21,12 +20,13 @@ namespace sp {
 
         void Update(const double overallProgress,
             const double stageProgress,
-            const double operationProgress,
             const char *const stage,
-            const char *const operation) {
+            const char *operation) override {
             Logf("VHACD %d (%s) %s", (int)(overallProgress + 0.5), stage, operation);
-        };
+        }
     };
+
+    static_assert(sizeof(VHACD::Triangle) == sizeof(glm::ivec3), "Unexpected vhacd triangle type");
 
     void decomposeConvexHullsForPrimitive(ConvexHullSet *set,
         const Gltf &model,
@@ -52,7 +52,7 @@ namespace sp {
 
         VHACD::IVHACD::Parameters params;
         params.m_callback = &vhacdCallback;
-        params.m_projectHullVertices = false;
+        // params.m_shrinkWrap = false;
         // params.m_oclAcceleration = false;
         // params.m_resolution = 1000000;
         // params.m_convexhullDownsampling = 8;
@@ -67,17 +67,17 @@ namespace sp {
         for (uint32 i = 0; i < interfaceVHACD->GetNConvexHulls(); i++) {
             VHACD::IVHACD::ConvexHull ihull;
             interfaceVHACD->GetConvexHull(i, ihull);
-            if (ihull.m_nPoints < 3 || ihull.m_nTriangles == 0) continue;
+            if (ihull.m_points.size() < 3 || ihull.m_triangles.empty()) continue;
 
             ConvexHull hull;
-            hull.points.resize(ihull.m_nPoints);
-
-            for (uint32 j = 0; j < ihull.m_nPoints; j++) {
-                hull.points[j] = glm::vec3(ihull.m_points[j * 3], ihull.m_points[j * 3 + 1], ihull.m_points[j * 3 + 2]);
+            hull.points.reserve(ihull.m_points.size());
+            for (auto &point : ihull.m_points) {
+                hull.points.emplace_back(point.mX, point.mY, point.mZ);
             }
 
-            auto src = reinterpret_cast<glm::ivec3 *>(ihull.m_triangles);
-            hull.triangles.assign(src, src + ihull.m_nTriangles);
+            hull.triangles.reserve(ihull.m_triangles.size());
+            auto src = reinterpret_cast<glm::ivec3 *>(ihull.m_triangles.data());
+            hull.triangles.assign(src, src + ihull.m_triangles.size());
 
             set->hulls.push_back(hull);
             Logf("Adding VHACD hull, %d points, %d triangles", hull.points.size(), hull.triangles.size());
@@ -85,28 +85,6 @@ namespace sp {
 
         interfaceVHACD->Clean();
         interfaceVHACD->Release();
-    }
-
-    void copyVhacdManifoldMeshToConvexHull(ConvexHull &hull, VHACD::TMMesh &mesh) {
-        const size_t nT = mesh.GetNTriangles();
-        const size_t nV = mesh.GetNVertices();
-
-        hull.points.resize(nV);
-        for (size_t v = 0; v < nV; v++) {
-            auto &pos = mesh.GetVertices().GetData().m_pos;
-            hull.points[v] = glm::vec3(pos[0], pos[1], pos[2]);
-            mesh.GetVertices().GetData().m_id = v;
-            mesh.GetVertices().Next();
-        }
-
-        hull.triangles.resize(nT);
-        for (size_t f = 0; f < nT; f++) {
-            auto &currentTriangle = mesh.GetTriangles().GetData();
-            hull.triangles[f].x = static_cast<int>(currentTriangle.m_vertices[0]->GetData().m_id);
-            hull.triangles[f].y = static_cast<int>(currentTriangle.m_vertices[1]->GetData().m_id);
-            hull.triangles[f].z = static_cast<int>(currentTriangle.m_vertices[2]->GetData().m_id);
-            mesh.GetTriangles().Next();
-        }
     }
 
     void buildConvexHullForPrimitive(ConvexHullSet *set,
@@ -117,34 +95,34 @@ namespace sp {
         set->decomposed = false;
 
         std::unordered_set<uint32_t> visitedIndexes;
-        std::vector<glm::vec3> points;
+        std::vector<VHACD::Vertex> points;
         points.reserve(prim.positionBuffer.Count());
 
         for (size_t i = 0; i < prim.indexBuffer.Count(); i++) {
             uint32_t index = prim.indexBuffer.Read(i);
             if (visitedIndexes.count(index) || index >= prim.positionBuffer.Count()) continue;
 
-            points.emplace_back(prim.positionBuffer.Read(index));
+            auto value = prim.positionBuffer.Read(index);
+            points.emplace_back(value.x, value.y, value.z);
             visitedIndexes.insert(index);
         }
 
-        btConvexHullComputer chc;
-        chc.compute(reinterpret_cast<const float *>(points.data()), sizeof(glm::vec3), points.size(), -1, -1);
+        VHACD::QuickHullImpl hullImpl;
+        hullImpl.computeConvexHull(points, 255 /* PhysX hull size limit */);
+        auto &vertices = hullImpl.getVertices();
+        auto &indices = hullImpl.getIndices();
+        if (vertices.size() < 3 || indices.empty()) return;
 
-        VHACD::ICHull icc;
-
-        for (int i = 0; i < chc.vertices.size(); i++) {
-            auto &v = chc.vertices[i];
-            VHACD::Vec3<double> point(v[0], v[1], v[2]);
-            icc.AddPoint(point);
+        auto &hull = set->hulls.emplace_back();
+        hull.points.reserve(vertices.size());
+        for (auto &point : vertices) {
+            hull.points.emplace_back(point.mX, point.mY, point.mZ);
         }
 
-        icc.Process(128, 0);
+        hull.triangles.reserve(indices.size());
+        auto src = reinterpret_cast<const glm::ivec3 *>(indices.data());
+        hull.triangles.assign(src, src + indices.size());
 
-        ConvexHull hull;
-        copyVhacdManifoldMeshToConvexHull(hull, icc.GetMesh());
-        if (hull.points.size() < 3 || hull.triangles.size() == 0) return;
-        set->hulls.push_back(hull);
         Logf("Adding simple hull, %d points, %d triangles", hull.points.size(), hull.triangles.size());
     }
 
