3157a9f2a11828a831bdb89c76b82bcc2cbcadde
Jacob Wirth <xthexder@users.noreply.github.com>
2022-09-01 15:23:07 -0400

    More test cases

--- tests/unit/is-float.cc
+++ tests/unit/is-float.cc
@@ -11,12 +11,15 @@ namespace IsFloatTests {
         AssertEqual(-0.01f, std::stof("-.01"), "Expected '-.01' to parse as -0.01");
 
         Assert(!sp::is_float(""), "Expected '' to be invalid");
+        Assert(!sp::is_float("foo"), "Expected 'foo' to be invalid");
         Assert(!sp::is_float("-"), "Expected '-' to be invalid");
         Assert(sp::is_float("1."), "Expected '1.' to be valid");
         Assert(sp::is_float(".01"), "Expected '.01' to be valid");
         Assert(sp::is_float("-.01"), "Expected '-' to be valid");
         Assert(sp::is_float("-1"), "Expected '-' to be valid");
         Assert(!sp::is_float("1.0a"), "Expected '1.0a' to be invalid");
+        Assert(!sp::is_float("1.0 2.0"), "Expected '1.0 2.0' to be invalid");
+        Assert(!sp::is_float("1.2.3.4"), "Expected '1.2.3.4' to be invalid");
         Assert(sp::is_float("123"), "Expected '123' to be valid");
         Assert(sp::is_float("123.345"), "Expected '123.456' to be valid");
         Assert(sp::is_float("-123.345"), "Expected '-123.456' to be valid");
