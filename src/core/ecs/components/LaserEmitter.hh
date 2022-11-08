d7ffdc75a63b8c3843568f87b5886f1d6ac941b1
Justin Li <git@justinli.net>
2022-11-08 17:26:40 -0500

    Add laser optical gate

--- src/core/ecs/components/LaserEmitter.hh
+++ src/core/ecs/components/LaserEmitter.hh
@@ -11,10 +11,12 @@ namespace ecs {
         float intensity = 1.0f; // multiplier applied to color to produce the final luminance
         sp::color_t color = glm::vec3(0); // HDR value, added to laser_color_* signal
         bool on = true;
+        float startDistance = 0.0f;
     };
 
     static Component<LaserEmitter> ComponentLaserEmitter("laser_emitter",
         ComponentField::New("intensity", &LaserEmitter::intensity),
         ComponentField::New("color", &LaserEmitter::color),
-        ComponentField::New("on", &LaserEmitter::on));
+        ComponentField::New("on", &LaserEmitter::on),
+        ComponentField::New("start_distance", &LaserEmitter::startDistance));
 } // namespace ecs
