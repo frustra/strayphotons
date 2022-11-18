6d2c8e8dbad277d2910a75b6cf150801ef478c66
Jacob Wirth <xthexder@users.noreply.github.com>
2022-11-18 00:38:31 -0500

    Change plugs to attach on drop rather than proximity

--- src/scripts/scripts/Magnets.cc
+++ src/scripts/scripts/Magnets.cc
@@ -3,12 +3,10 @@
 #include "ecs/EntityRef.hh"
 
 #include <glm/gtx/component_wise.hpp>
+#include <unordered_set>
 
 namespace sp::scripts {
     using namespace ecs;
-
-    static CVar<float> CVarMaxMagentForce("i.MaxMagnetForce", 1.0f, "Maximum force applied to magnetic objects");
-    static CVar<float> CVarMaxMagentTorque("i.MaxMagnetTorque", 0.01f, "Maximum torque applied to magnetic objects");
 
     std::array magnetScripts = {
         InternalScript(
@@ -18,42 +16,54 @@ namespace sp::scripts {
                 auto &joints = ent.Get<PhysicsJoints>(lock);
 
                 struct ScriptData {
-                    Entity socketEntity;
-                    std::vector<Entity> grabEntities;
+                    Entity attachedEntity;
+                    robin_hood::unordered_flat_set<Entity> grabEntities;
+                    robin_hood::unordered_flat_set<Entity> socketEntities;
                 };
                 ScriptData scriptData = {};
                 if (state.userData.has_value()) scriptData = std::any_cast<ScriptData>(state.userData);
 
                 Event event;
                 while (EventInput::Poll(lock, state.eventQueue, event)) {
-                    if (event.name == "/magnet/attach") {
-                        PhysicsJoint joint;
-                        joint.target = event.source;
-                        joint.type = PhysicsJointType::Fixed;
-                        // joint.limit = glm::vec2(CVarMaxMagentForce.Get(), CVarMaxMagentTorque.Get());
-                        joints.Add(joint);
-
-                        scriptData.socketEntity = event.source;
-
-                        for (auto &grabEntity : scriptData.grabEntities) {
-                            EventBindings::SendEvent(lock, grabEntity, Event{INTERACT_EVENT_INTERACT_GRAB, ent, false});
+                    if (event.name == "/magnet/nearby") {
+                        if (std::holds_alternative<bool>(event.data)) {
+                            if (std::get<bool>(event.data)) {
+                                scriptData.socketEntities.emplace(event.source);
+                            } else {
+                                scriptData.socketEntities.erase(event.source);
+                            }
                         }
                     } else if (event.name == INTERACT_EVENT_INTERACT_GRAB) {
                         if (std::holds_alternative<bool>(event.data)) {
                             // Grab(false) = Drop
-                            sp::erase(scriptData.grabEntities, event.source);
+                            if (!std::get<bool>(event.data)) {
+                                scriptData.grabEntities.erase(event.source);
+
+                                if (scriptData.grabEntities.empty() && !scriptData.socketEntities.empty()) {
+                                    // Attach to a nearby socket if one exists
+                                    // TODO: Sort by proximity when multiple are nearby
+                                    scriptData.attachedEntity = *scriptData.socketEntities.begin();
+
+                                    PhysicsJoint joint;
+                                    joint.target = scriptData.attachedEntity;
+                                    joint.type = PhysicsJointType::Fixed;
+                                    joints.Add(joint);
+                                }
+                            }
                         } else if (std::holds_alternative<Transform>(event.data)) {
-                            if (scriptData.socketEntity) {
-                                Debugf("Detached: %s from %s",
+                            if (scriptData.attachedEntity) {
+                                Debugf("Detaching: %s from %s",
                                     ecs::ToString(lock, ent),
-                                    ecs::ToString(lock, scriptData.socketEntity));
+                                    ecs::ToString(lock, scriptData.attachedEntity));
 
                                 sp::erase_if(joints.joints, [&](auto &&joint) {
-                                    return joint.target == scriptData.socketEntity;
+                                    return joint.target == scriptData.attachedEntity;
                                 });
+
+                                scriptData.attachedEntity = {};
                             }
 
-                            scriptData.grabEntities.emplace_back(event.source);
+                            scriptData.grabEntities.emplace(event.source);
                         } else {
                             Errorf("Unsupported grab event type: %s", event.toString());
                         }
@@ -62,7 +72,7 @@ namespace sp::scripts {
 
                 state.userData = scriptData;
             },
-            "/magnet/attach",
+            "/magnet/nearby",
             INTERACT_EVENT_INTERACT_GRAB),
         InternalScript(
             "magnetic_socket",
@@ -87,12 +97,12 @@ namespace sp::scripts {
                     if (event.name == "/trigger/magnetic/leave") {
                         if (event.source == enableTrigger) {
                             scriptData.disabledEntities.erase(*data);
+                            EventBindings::SendEvent(lock, *data, Event{"/magnet/nearby", ent, false});
                         }
                     } else if (event.name == "/trigger/magnetic/enter") {
                         if (event.source == ent && !scriptData.disabledEntities.contains(*data)) {
-                            Debugf("Attached: %s to %s", ecs::ToString(lock, *data), ecs::ToString(lock, event.source));
                             scriptData.disabledEntities.emplace(*data);
-                            EventBindings::SendEvent(lock, *data, Event{"/magnet/attach", ent, true});
+                            EventBindings::SendEvent(lock, *data, Event{"/magnet/nearby", ent, true});
                         }
                     }
                 }
