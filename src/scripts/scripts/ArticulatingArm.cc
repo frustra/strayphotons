3528abdee18f7d121dd03261fcd33b7f59c1ba7c
Jacob Wirth <xthexder@users.noreply.github.com>
2022-12-23 17:07:58 -0500

    Convert more scripts. Allow sockets to set their snap angle

--- src/scripts/scripts/ArticulatingArm.cc
+++ src/scripts/scripts/ArticulatingArm.cc
@@ -5,20 +5,16 @@
 namespace sp::scripts {
     using namespace ecs;
 
-    InternalScript articulatingArm("articulating_arm",
-        [](ScriptState &state, Lock<WriteAll> lock, Entity ent, chrono_clock::duration interval) {
+    struct ArticulatingArm {
+        bool locked = false;
+
+        void OnTick(ScriptState &state, Lock<WriteAll> lock, Entity ent, chrono_clock::duration interval) {
             if (!ent.Has<ecs::Name, ecs::SignalBindings>(lock)) return;
 
             float lockedRatio = SignalBindings::GetSignal(lock, ent, "locked_ratio");
 
-            struct ScriptData {
-                bool locked = false;
-            };
-            ScriptData scriptData = {};
-            if (state.userData.has_value()) scriptData = std::any_cast<ScriptData>(state.userData);
-
             bool shouldLock = lockedRatio > 0.999;
-            if (shouldLock == scriptData.locked) return;
+            if (shouldLock == locked) return;
 
             auto rootName = ent.Get<ecs::Name>(lock);
 
@@ -54,14 +50,16 @@ namespace sp::scripts {
                     joint.type = ecs::PhysicsJointType::Fixed;
                     joint.localOffset = child.Get<ecs::TransformSnapshot>(lock).GetInverse() *
                                         ent.Get<ecs::TransformSnapshot>(lock);
-                } else if (scriptData.locked) {
+                } else if (locked) {
                     sp::erase_if(joints, [ent](auto &&arg) {
                         return arg.target == ent && arg.type == ecs::PhysicsJointType::Fixed;
                     });
                 }
             }
 
-            scriptData.locked = shouldLock;
-            state.userData = scriptData;
-        });
+            locked = shouldLock;
+        }
+    };
+    StructMetadata MetadataArticulatingArm(typeid(ArticulatingArm));
+    InternalScript2<ArticulatingArm> articulatingArm("articulating_arm", MetadataArticulatingArm);
 } // namespace sp::scripts
