6d519aadc8cce82ec0b293ad8d9d52cb934aab8c
Kevin Nause <kevin@nause.engineering>
2023-08-03 11:31:08 -0700

    Android build support

--- rust/bin/demo/src/lib.rs
+++ rust/bin/demo/src/lib.rs
@@ -1,10 +1,5 @@
 use mobile_entry_point::mobile_entry_point;
 use std::error::Error;
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
 
 #[cfg(target_os = "android")]
 fn init_logging() -> Result<(), Box<dyn Error>> {
@@ -30,23 +25,7 @@ fn start() -> Result<(), Box<dyn Error>> {
     // API for setting event_loop (closure to be invoked by c++)
     // API for setting scripts/assets
 
-    let event_loop = EventLoop::new();
-
-    let _window = WindowBuilder::new()
-        .with_title("A fantastic window!")
-        .with_inner_size(winit::dpi::LogicalSize::new(128.0, 128.0))
-        .build(&event_loop)
-        .unwrap();
-
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
-            ..
-        } => {
-            *control_flow = ControlFlow::Exit;
-        }
-        _ => (),
-    });
+    Ok(())
 }
 
 #[mobile_entry_point]
