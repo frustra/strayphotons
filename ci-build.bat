25ab3eb6c6a1346bc42449e695243470e2ad44a0
Jacob Wirth <xthexder@users.noreply.github.com>
2024-12-20 00:14:30 -0800

    Update ci-build.bat

--- ci-build.bat
+++ ci-build.bat
@@ -1,3 +1,3 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 "C:\Program Files\Git\bin\bash.exe" -c "./ci-build.sh"
