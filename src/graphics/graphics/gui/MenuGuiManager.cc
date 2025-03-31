1e7173f17bc10d1a9f9725065602263d2349829a
Jacob Wirth <xthexder@users.noreply.github.com>
2025-03-31 01:02:00 -0700

    Adjust menu connection point to station center so gravity is slightly down

--- src/graphics/graphics/gui/MenuGuiManager.cc
+++ src/graphics/graphics/gui/MenuGuiManager.cc
@@ -361,17 +361,23 @@ namespace sp {
         }
     }
 
+    template<typename TP>
+    std::time_t to_time_t(TP tp) {
+        using namespace std::chrono;
+        auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now() + system_clock::now());
+        return system_clock::to_time_t(sctp);
+    }
+
     void MenuGuiManager::RefreshSaveList() {
         saveList.clear();
         int i = 0;
         while (std::filesystem::exists("./saves/save" + std::to_string(i) + ".json")) {
             auto last_write_time = std::filesystem::last_write_time("./saves/save" + std::to_string(i) + ".json");
-            std::time_t time = std::chrono::system_clock::to_time_t(
-                std::chrono::clock_cast<std::chrono::system_clock>(last_write_time));
+            std::time_t time = to_time_t(last_write_time);
             std::tm *tm = std::localtime(&time);
 
             std::string timeStr(100, '\0');
-            size_t n = std::strftime(timeStr.data(), timeStr.size(), "%x %X", tm);
+            size_t n = std::strftime(timeStr.data(), timeStr.size(), "%F %X", tm);
             timeStr.resize(n);
             saveList.emplace_back("Save " + std::to_string(i) + ": " + timeStr, "save" + std::to_string(i));
             i++;
