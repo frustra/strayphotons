9941dea0f37f7b2b986c3887d90d919fc682b4ab
Jacob Wirth <xthexder@users.noreply.github.com>
2026-03-08 21:33:08 -0700

    Update libsoundio for linux

--- src/audio/audio/AudioManager.cc
+++ src/audio/audio/AudioManager.cc
@@ -12,51 +12,91 @@
 #include "console/CVar.hh"
 #include "ecs/EcsImpl.hh"
 #include "game/GameEntities.hh"
+#include "strayphotons/cpp/Utility.hh"
 
 #include <memory>
 #include <resonance_audio/api/resonance_audio_api.h>
 #include <resonance_audio/base/constants_and_types.h>
 #include <soundio/soundio.h>
+#include <tracy/Tracy.hpp>
 
 namespace sp {
     static CVar<float> CVarVolume("s.Volume", 1.0f, "Global volume control");
+    static CVar<int> CVarAudioBackend("s.AudioBackend",
+        0,
+        "Audio backend (0: JACK, 1: PulseAudio, 2: ALSA, 3: CoreAudio: 4: Wasapi)");
 
     AudioManager::AudioManager()
         : RegisteredThread("AudioManager", std::chrono::milliseconds(20), false), sampleRate(48000),
           decoderQueue("AudioDecode") {
 
-        framesPerBuffer = sampleRate * interval.count() / 1e9;
-        Assertf(framesPerBuffer < vraudio::kMaxSupportedNumFrames, "buffer too big: %d", framesPerBuffer);
+        framesPerBuffer = std::min(vraudio::kMaxSupportedNumFrames,
+            (size_t)CeilToPowerOfTwo(sampleRate * interval.count() / 1e9));
         Assertf(framesPerBuffer >= 32, "buffer too small: %d", framesPerBuffer); // FftManager::kMinFftSize
+        Logf("Audio SampleRate: %llu (%llu frames per buffer)", sampleRate, framesPerBuffer);
 
-        resonance.reset(vraudio::CreateResonanceAudioApi(2, framesPerBuffer, sampleRate));
+        // Allow cycling through each audio backend 1 time
+        maxRestartAttempts = (int)SoundIoBackendDummy - (int)SoundIoBackendNone;
 
         {
             auto lock = ecs::StartTransaction<ecs::AddRemove>();
             soundObserver = lock.Watch<ecs::ComponentAddRemoveEvent<ecs::Audio>>();
         }
 
+        resonance.reset(vraudio::CreateResonanceAudioApi(2, framesPerBuffer, sampleRate));
+
         StartThread();
     }
 
-    void AudioManager::AudioErrorCallback(SoundIoOutStream *outstream, int error) {
-        Errorf("Shutting down audio manager: libsoundio error: %s", soundio_strerror(error));
+    std::vector<const char *> AudioManager::GetBackendNames() const {
+        if (!soundio) return {};
+        std::vector<const char *> result;
+        result.reserve(soundio_backend_count(soundio));
+        for (size_t i = 0; i < result.capacity(); i++) {
+            SoundIoBackend backend = soundio_get_backend(soundio, i);
+            if (backend != SoundIoBackendDummy) result.emplace_back(soundio_backend_name(backend));
+        }
+        return result;
+    }
 
-        auto self = static_cast<AudioManager *>(outstream->userdata);
-        self->Shutdown(true);
+    void AudioManager::AudioErrorCallback(SoundIoOutStream *outstream, int error) {
+        Errorf("AudioManager: libsoundio error: %s", soundio_strerror(error));
+
+        if (!attemptRestart && restartAttempt < maxRestartAttempts) {
+            attemptRestart = true;
+            restartAttempt++;
+        }
+        Shutdown(false);
     }
 
     bool AudioManager::ThreadInit() {
         ZoneScoped;
 
         soundio = soundio_create();
+        soundio->app_name = "Stray Photons";
 
-        int err = soundio_connect(soundio);
+        int err = 0;
+        int backendCount = soundio_backend_count(soundio);
+        int selectedBackend = CVarAudioBackend.Get(true) + restartAttempt;
+        for (int i = 0; i < backendCount; i++) {
+            selectedBackend = (selectedBackend + i) % backendCount;
+            SoundIoBackend backend = soundio_get_backend(soundio, selectedBackend);
+            err = soundio_connect_backend(soundio, backend);
+            if (!err) {
+                Logf("Using audio backend: %s", soundio_backend_name(backend));
+                break;
+            } else {
+                Logf("Audio Backend %s error: %s", soundio_backend_name(backend), soundio_strerror(err));
+            }
+        }
         if (err) {
             Errorf("soundio_connect: %s", soundio_strerror(err));
             return false;
         }
-
+        if (selectedBackend != CVarAudioBackend.Get()) {
+            CVarAudioBackend.Set(selectedBackend);
+            CVarAudioBackend.Get(true);
+        }
         soundio_flush_events(soundio);
 
         deviceIndex = soundio_default_output_device_index(soundio);
@@ -72,11 +112,18 @@ namespace sp {
             Shutdown(false);
             return false;
         }
+        Logf("Selected audio device: %s", device->name);
 
         outstream = soundio_outstream_create(device);
         outstream->format = SoundIoFormatFloat32NE;
-        outstream->write_callback = AudioWriteCallback;
-        outstream->error_callback = AudioErrorCallback;
+        outstream->write_callback = [](SoundIoOutStream *outstream, int frameCountMin, int frameCountMax) {
+            auto self = static_cast<AudioManager *>(outstream->userdata);
+            self->AudioWriteCallback(outstream, frameCountMin, frameCountMax);
+        };
+        outstream->error_callback = [](SoundIoOutStream *outstream, int error) {
+            auto self = static_cast<AudioManager *>(outstream->userdata);
+            self->AudioErrorCallback(outstream, error);
+        };
         outstream->userdata = this;
         outstream->sample_rate = sampleRate;
 
@@ -105,17 +152,31 @@ namespace sp {
             Shutdown(false);
             return false;
         }
+
+        (void)soundio_outstream_clear_buffer(outstream);
         return true;
     }
 
     void AudioManager::Shutdown(bool waitForExit) {
         StopThread(waitForExit);
+    }
+
+    void AudioManager::ThreadShutdown() {
         if (outstream) soundio_outstream_destroy(outstream);
         if (device) soundio_device_unref(device);
         if (soundio) soundio_destroy(soundio);
         outstream = nullptr;
         device = nullptr;
         soundio = nullptr;
+
+        if (attemptRestart) {
+            attemptRestart = false;
+            Logf("Attempting audio thread restart (attempt %d/%d)", restartAttempt, maxRestartAttempts);
+            decoderQueue.Dispatch<void>([this] {
+                StopThread();
+                StartThread();
+            });
+        }
     }
 
     AudioManager::~AudioManager() {
@@ -127,6 +188,15 @@ namespace sp {
         {
             ZoneScopedN("soundio_flush_events");
             soundio_flush_events(soundio);
+        }
+        if (CVarAudioBackend.Changed()) {
+            CVarAudioBackend.Get(true);
+            restartAttempt = 0;
+            Logf("Restarting audio system (backend changed)");
+            decoderQueue.Dispatch<void>([this] {
+                StopThread();
+                StartThread();
+            });
         }
         SyncFromECS();
     }
@@ -148,18 +218,17 @@ namespace sp {
         while (soundObserver.Poll(lock, compEvent)) {
             if (compEvent.type == Tecs::EventType::ADDED) {
                 if (!compEvent.entity.Has<ecs::EventInput, ecs::Audio>(lock)) continue;
-                ecs::QueueTransaction<ecs::Write<ecs::Audio, ecs::EventInput>>(
-                    [this, ent = compEvent.entity](auto &lock) {
-                        if (!ent.Has<ecs::EventInput, ecs::Audio>(lock)) return;
+                ecs::QueueTransaction<ecs::Write<ecs::Audio, ecs::EventInput>>([ent = compEvent.entity](auto &lock) {
+                    if (!ent.Has<ecs::EventInput, ecs::Audio>(lock)) return;
 
-                        auto &audio = ent.Get<ecs::Audio>(lock);
-                        if (!audio.eventQueue) audio.eventQueue = ecs::EventQueue::New();
-                        auto &eventInput = ent.Get<ecs::EventInput>(lock);
-                        eventInput.Register(lock, audio.eventQueue, "/sound/play");
-                        eventInput.Register(lock, audio.eventQueue, "/sound/resume");
-                        eventInput.Register(lock, audio.eventQueue, "/sound/pause");
-                        eventInput.Register(lock, audio.eventQueue, "/sound/stop");
-                    });
+                    auto &audio = ent.Get<ecs::Audio>(lock);
+                    if (!audio.eventQueue) audio.eventQueue = ecs::EventQueue::New();
+                    auto &eventInput = ent.Get<ecs::EventInput>(lock);
+                    eventInput.Register(lock, audio.eventQueue, "/sound/play");
+                    eventInput.Register(lock, audio.eventQueue, "/sound/resume");
+                    eventInput.Register(lock, audio.eventQueue, "/sound/pause");
+                    eventInput.Register(lock, audio.eventQueue, "/sound/stop");
+                });
             } else if (compEvent.type == Tecs::EventType::REMOVED) {
 
                 auto *entSound = soundEntityMap.find(compEvent.entity);
@@ -261,7 +330,7 @@ namespace sp {
             while (ecs::EventInput::Poll(lock, sources.eventQueue, event)) {
                 int *ptr = ecs::EventData::TryGet<int>(event.data);
                 int index = ptr ? *ptr : 0;
-                if (index >= soundIDs->size()) continue;
+                if (index >= (int)soundIDs->size()) continue;
                 auto soundID = soundIDs->at(index);
 
                 if (event.name == "/sound/play") {
@@ -293,10 +362,9 @@ namespace sp {
         }
 
         ZoneScoped;
-        auto self = static_cast<AudioManager *>(outstream->userdata);
 
-        self->soundEvents.TryPollEvents([&](const SoundEvent &event) {
-            auto &sound = self->sounds.Get(event.soundID);
+        soundEvents.TryPollEvents([&](const SoundEvent &event) {
+            auto &sound = sounds.Get(event.soundID);
             switch (event.type) {
             case SoundEvent::Type::PlayFromStart:
                 sound.bufferOffset = 0;
@@ -311,68 +379,101 @@ namespace sp {
             }
         });
 
+        if (!resonance) return;
+
         struct SoundIoChannelArea *areas;
-        int framesPerBuffer = self->framesPerBuffer;
-        int framesToWrite = framesPerBuffer * std::max(std::min(1, frameCountMax / framesPerBuffer),
-                                                  (frameCountMin + framesPerBuffer - 1) / framesPerBuffer);
-        if (framesToWrite <= 0) return;
-        int err = soundio_outstream_begin_write(outstream, &areas, &framesToWrite);
-
         int channelCount = outstream->layout.channel_count;
-        auto basePtr = reinterpret_cast<float *>(areas[0].ptr);
+        int framesToWrite = std::max(frameCountMin, std::min(frameCountMax, (int)framesPerBuffer));
 
-        for (int channel = 0; channel < channelCount; channel++) {
-            Assert(areas[channel].step == (int)sizeof(float) * channelCount, "expected interleaved output buffer");
-            Assert((float *)areas[channel].ptr == basePtr + channel, "expected interleaved output buffer");
+        // double out_latency = -1;
+        // int err = soundio_outstream_get_latency(outstream, &out_latency);
+        // Logf("frameCountMin %d frameCountMax %d framesPerBuffer %d framesToWrite %d latency %f (%s)",
+        //     frameCountMin,
+        //     frameCountMax,
+        //     framesPerBuffer,
+        //     framesToWrite,
+        //     out_latency,
+        //     soundio_strerror(err));
+
+        while ((int)std::min(outputBuffers[0].size(), outputBuffers[1].size()) < framesToWrite) {
+            ZoneScopedN("Render");
+            // Logf("Render outputBuffers.size() %llu / %d",
+            //     std::min(outputBuffers[0].size(), outputBuffers[1].size()),
+            //     framesToWrite);
+
+            outputBuffers[0].resize(outputBuffers[0].size() + framesPerBuffer);
+            outputBuffers[1].resize(outputBuffers[1].size() + framesPerBuffer);
+            float *outputPtrs[2] = {
+                outputBuffers[0].data() + outputBuffers[0].size() - framesPerBuffer,
+                outputBuffers[1].data() + outputBuffers[1].size() - framesPerBuffer,
+            };
+
+            auto validIndexPtr = sounds.GetValidIndexes();
+            for (auto soundID : *validIndexPtr) {
+                auto &source = sounds.Get(soundID);
+                if (!source.play || !source.audioBuffer->Ready()) continue;
+
+                auto &audioBuffer = *source.audioBuffer->Get();
+                auto floatsRemaining = audioBuffer.samples.size() - source.bufferOffset;
+
+                auto framesRemaining = std::min((size_t)framesPerBuffer, floatsRemaining / audioBuffer.channelCount);
+
+                resonance->SetInterleavedBuffer(source.resonanceID,
+                    &audioBuffer.samples[source.bufferOffset],
+                    audioBuffer.channelCount,
+                    framesRemaining);
+
+                auto floatsPerSourceBuffer = framesPerBuffer * audioBuffer.channelCount;
+                source.bufferOffset += floatsPerSourceBuffer;
+                if (source.bufferOffset >= audioBuffer.samples.size()) {
+                    source.bufferOffset = 0;
+                    if (!source.loop) source.play = false;
+                }
+            }
+
+            resonance->FillPlanarOutputBuffer(channelCount, framesPerBuffer, outputPtrs);
         }
 
-        auto floatsPerBuffer = framesPerBuffer * channelCount;
-        auto outputBuffer = (float *)areas[0].ptr;
-        const float *lastSample = Zeros;
+        int framesWritten = 0;
+        while (framesWritten < framesToWrite) {
+            ZoneScopedN("CopyOutput");
+            int batchSize = framesToWrite;
+            int err = soundio_outstream_begin_write(outstream, &areas, &batchSize);
+            if (err) {
+                Errorf("soundio begin_write error %s", soundio_strerror(err));
+                return;
+            }
+            // Logf("CopyOutput framesToWrite %d batchSize %d framesWritten %d", framesToWrite, batchSize,
+            // framesWritten);
 
-        while (framesToWrite >= framesPerBuffer) {
-            if (self->resonance) {
-                auto validIndexPtr = self->sounds.GetValidIndexes();
-                for (auto soundID : *validIndexPtr) {
-                    auto &source = self->sounds.Get(soundID);
-                    if (!source.play || !source.audioBuffer->Ready()) continue;
-
-                    auto &audioBuffer = *source.audioBuffer->Get();
-                    auto floatsRemaining = audioBuffer.samples.size() - source.bufferOffset;
-
-                    auto framesRemaining = std::min((size_t)framesPerBuffer,
-                        floatsRemaining / audioBuffer.channelCount);
-
-                    self->resonance->SetInterleavedBuffer(source.resonanceID,
-                        &audioBuffer.samples[source.bufferOffset],
-                        audioBuffer.channelCount,
-                        framesRemaining);
-
-                    auto floatsPerSourceBuffer = framesPerBuffer * audioBuffer.channelCount;
-                    source.bufferOffset += floatsPerSourceBuffer;
-                    if (source.bufferOffset >= audioBuffer.samples.size()) {
-                        source.bufferOffset = 0;
-                        if (!source.loop) source.play = false;
+            bool interleaved = areas[0].step == (int)sizeof(float) * channelCount;
+            Assert(interleaved || areas[0].step == (int)sizeof(float), "expected linear audio output buffer");
+            size_t count = std::min({(size_t)batchSize, outputBuffers[0].size(), outputBuffers[1].size()});
+            if (count > 0) {
+                if (interleaved) {
+                    auto outputPtr = reinterpret_cast<float *>(areas[0].ptr);
+                    for (size_t i = 0; i < count; i++) {
+                        *outputPtr++ = outputBuffers[0][i];
+                        *outputPtr++ = outputBuffers[1][i];
+                    }
+                } else {
+                    for (int channel = 0; channel < channelCount; channel++) {
+                        auto channelPtr = reinterpret_cast<float *>(areas[channel].ptr);
+                        size_t count = std::min((size_t)batchSize, outputBuffers[channel].size());
+                        std::copy_n(outputBuffers[channel].begin(), count, channelPtr);
                     }
                 }
-
-                ZoneScopedN("Render");
-                self->resonance->FillInterleavedOutputBuffer(channelCount, framesPerBuffer, outputBuffer);
-            } else {
-                std::fill(outputBuffer, outputBuffer + floatsPerBuffer, 0);
+                outputBuffers[0].erase(outputBuffers[0].begin(), outputBuffers[0].begin() + count);
+                outputBuffers[1].erase(outputBuffers[1].begin(), outputBuffers[1].begin() + count);
+                framesWritten += count;
             }
-            outputBuffer += floatsPerBuffer;
-            framesToWrite -= framesPerBuffer;
-            lastSample = outputBuffer - channelCount;
-        }
 
-        while (framesToWrite > 0) {
-            std::copy(lastSample, lastSample + channelCount, outputBuffer);
-            outputBuffer += channelCount;
-            framesToWrite--;
+            err = soundio_outstream_end_write(outstream);
+            if (err) {
+                Errorf("soundio end_write error %s", soundio_strerror(err));
+                return;
+            }
         }
-
-        err = soundio_outstream_end_write(outstream);
-        Assertf(!err, "soundio end_write error %s", soundio_strerror(err));
+        // Logf("Finished bufferSize[0] %llu bufferSize[1] %llu", outputBuffers[0].size(), outputBuffers[1].size());
     }
 } // namespace sp
