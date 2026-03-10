bc50cb91ed2d4bedc211f52616708646eebb8af7
Jacob Wirth <xthexder@users.noreply.github.com>
2026-03-10 01:23:21 -0700

    Clean up audio library cmake warnings

--- ext/libnyquist.cmake
+++ ext/libnyquist.cmake
@@ -7,8 +7,10 @@
 
 set(BUILD_EXAMPLE OFF CACHE INTERNAL "")
 
+add_subdirectory(libnyquist)
+
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    add_compile_options(/wd4028 /wd4245 /wd4456 /wd4514 /wd4701 /wd4702 /wd4706)
+    target_compile_options(libnyquist PRIVATE /wd4028 /wd4113 /wd4245 /wd4456 /wd4514 /wd4701 /wd4702 /wd4706)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-stringop-overread")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-reorder -Wno-unused-variable -Wno-sign-compare -Wno-range-loop-construct")
@@ -16,5 +18,3 @@ elseif(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-shift-negative-value -Wno-macro-redefined -Wno-static-in-inline")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-reorder-ctor -Wno-unused-private-field -Wno-range-loop-construct -Wno-tautological-overlap-compare -Wno-invalid-utf8")
 endif()
-
-add_subdirectory(libnyquist)
--- ext/resonance-audio.cmake
+++ ext/resonance-audio.cmake
@@ -1,5 +1,5 @@
 #
-# Stray Photons - Copyright (C) 2023 Jacob Wirth & Justin Li
+# Stray Photons - Copyright (C) 2026 Jacob Wirth & Justin Li
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 # If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -10,35 +10,18 @@ set(BUILD_RESONANCE_AUDIO_API OFF CACHE INTERNAL "")
 set(EIGEN3_DIR "${CMAKE_CURRENT_SOURCE_DIR}/eigen/" CACHE PATH "")
 set(PFFFT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/pffft/" CACHE PATH "")
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    foreach(flag_var CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG)
-        STRING(REGEX REPLACE "/RTC[^ ]*" "" ${flag_var} "${${flag_var}}")
-    endforeach(flag_var)
-endif()
-
 add_subdirectory(resonance-audio)
 
-add_library(ResonanceAudioStatic STATIC $<TARGET_OBJECTS:ResonanceAudioObj>
-        $<TARGET_OBJECTS:SadieHrtfsObj>
-        $<TARGET_OBJECTS:PffftObj>)
+add_library(ResonanceAudioStatic STATIC
+    $<TARGET_OBJECTS:ResonanceAudioObj>
+    $<TARGET_OBJECTS:SadieHrtfsObj>
+    $<TARGET_OBJECTS:PffftObj>
+)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    target_compile_options(ResonanceAudioObj PRIVATE "/wd4127")
-    target_compile_options(ResonanceAudioObj PRIVATE "/O2")
-    target_compile_options(SadieHrtfsObj PRIVATE "/O2")
-    target_compile_options(PffftObj PRIVATE "/O2")
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    target_compile_options(ResonanceAudioObj PRIVATE "-O3")
-    target_compile_options(SadieHrtfsObj PRIVATE "-O3")
-    target_compile_options(PffftObj PRIVATE "-O3")
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$")
-    target_compile_options(ResonanceAudioObj PRIVATE "-O3")
-    target_compile_options(SadieHrtfsObj PRIVATE "-O3")
-    target_compile_options(PffftObj PRIVATE "-O3")
+    target_compile_options(ResonanceAudioObj PRIVATE /wd4127 /wd4702)
 endif()
 
-target_include_directories(
-    ResonanceAudioStatic
-    INTERFACE
-        ./resonance-audio/
+target_include_directories(ResonanceAudioStatic INTERFACE
+    ./resonance-audio/
 )
